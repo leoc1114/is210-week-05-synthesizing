c165874b81fc9fc9b44feacaf5cdf344a4a33b02
Wei Chen <wei.chen@spsmail.cuny.edu>
2015-02-28 22:33:09 +0000

    modified task 1

---
+++ task_01.py
@@ -0,0 +1,62 @@
+#! usr/bin/env python
+# _*_ coding: utf-8 _*_
+"""Created Task 1"""
+
+
+import decimal
+
+ABSOLUTE_DIFFERENCE = decimal.Decimal (273.15)
+
+def fahrenheit_to_celsius(fahrenheit_degrees):
+    """converting fahrenheit degrees to celsius degrees.
+
+    Args:
+        fahrenheit_degrees(mix): to convert fahrenheit to celsius
+
+    Returns:
+        a number in decimal
+
+    Examples:
+        >>>fahrenheit_to_celsius(300)
+        Decimal('148')
+        
+    """
+        
+    celsius_degrees = (fahrenheit_degrees - 32) * 5 / 9
+    return decimal.Decimal (celsius_degrees)
+
+def celsius_to_kelvin(celsius_degrees):
+    """converting celsius degrees to kelvin degrees.
+
+    Args:
+        celsius_dgrees(mix): to convert celsius to kelvin
+
+    Returns:
+        a number in decimal
+
+    Examples:
+        >>>celsius_to_kelvin(100)
+        Decimal('373.14......')
+        
+    """
+    
+    kelvin_degrees = celsius_degrees + ABSOLUTE_DIFFERENCE
+    return decimal.Decimal (kelvin_degrees)
+
+def fahrenheit_to_kelvin(fahrenheit_degrees):
+    """Converting fahrenheit to kelvin.
+
+    Args:
+        fahrenheit_degrees(mix): to convert fahrenheit to kelvin
+
+    Returns:
+        a number in decimal
+
+    Examples:
+        >>>fahrenheit_to_kelvin(212)
+        Decimal('373.14...')
+        
+    """
+
+    kelvin_degrees = (fahrenheit_degrees - 32) * 5 / 9 + ABSOLUTE_DIFFERENCE
+    return decimal.Decimal (kelvin_degrees)
