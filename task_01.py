2ddea3332ea0919f53b8e2d674153bf84d660a2f
Wei Chen <wei.chen@spsmail.cuny.edu>
2015-03-01 02:11:54 +0000

    modified task 1

--- task_01.py
+++ task_01.py
@@ -21,7 +21,6 @@ def fahrenheit_to_celsius(fahrenheit_degrees):
         >>>fahrenheit_to_celsius(212)
         Decimal('100')
     """
-        
     celsius_degrees = (decimal.Decimal(fahrenheit_degrees) - 32) * 5 / 9
     return decimal.Decimal(celsius_degrees)
 
@@ -39,7 +38,6 @@ def celsius_to_kelvin(celsius_degrees):
         >>>celsius_to_kelvin(100)
         Decimal('373.15')
     """
-    
     kelvin_degrees = celsius_degrees + ABSOLUTE_DIFFERENCE
     return decimal.Decimal(kelvin_degrees)
 
@@ -57,6 +55,6 @@ def fahrenheit_to_kelvin(fahrenheit_degrees):
         >>>fahrenheit_to_kelvin(212)
         Decimal('373.15')    
     """
-
-    kelvin_degrees = (decimal.Decimal(fahrenheit_degrees) - 32) * 5 / 9 + ABSOLUTE_DIFFERENCE
+    kelvin_degrees = ((decimal.Decimal(fahrenheit_degrees) - 32) * 5 / 9
+    + ABSOLUTE_DIFFERENCE)
     return decimal.Decimal(kelvin_degrees)
